ccd0acbedc227ef9780fca15e390c186919f3c2c
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-07-07 22:34:43 -0400

    database is added.

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
